77391d4e41b5b736eedb4c19074fd98f8da5ff71
amala <amalakkad93@outlook.com>
2023-07-11 13:44:35 -0700

    fix DB on Booking

--- backend/db/migrations/20230709210002-create-booking.js
+++ backend/db/migrations/20230709210002-create-booking.js
@@ -18,6 +18,7 @@ module.exports = {
       spotId: {
         type: Sequelize.INTEGER,
         allowNull: false,
+        references: { model: 'Spots' },
         onDelete: 'CASCADE',
         hooks: true
       },
