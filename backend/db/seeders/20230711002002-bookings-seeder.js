7cabd1a74fa00df3c32b7a7890ccc6b589822116
amala <amalakkad93@outlook.com>
2023-07-17 00:00:07 -0700

    fix booking seeder

--- backend/db/seeders/20230711002002-bookings-seeder.js
+++ backend/db/seeders/20230711002002-bookings-seeder.js
@@ -72,73 +72,66 @@
 //     }, {});
 //   }
 // };
-
 'use strict';
-const { Booking } = require('../models');
-const bcrypt = require("bcryptjs");
-
+const { Booking } = require('../models')
+/** @type {import('sequelize-cli').Migration} */
 
 let options = {};
 if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;  // define your schema in options object
+  options.schema = process.env.SCHEMA; // define your schema in options object
 }
-/** @type {import('sequelize-cli').Migration} */
+
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
     await Booking.bulkCreate([
-      {
-        spotId: 1,
-        userId: 1,
-        startDate: '2023-08-12',
-        endDate: '2023-08-16',
+{
+    spotId: 1,
+    userId: 1,
+    startDate: '2025-08-12',
+    endDate: '2025-08-16',
+
+
       },
       {
         spotId: 2,
         userId: 1,
-        startDate: '2023-07-14',
-        endDate: '2023-08-16',
-      },
-      {
-        spotId: 3,
-        userId: 1,
         startDate: '1985-07-14',
         endDate: '1986-08-16',
+      },
+      {
+        spotId: 1,
+        userId: 2,
+        startDate: '2026-07-14',
+        endDate: '2026-08-16',
       },
       {
         spotId: 2,
         userId: 2,
         startDate: '2023-09-15',
         endDate: '2023-09-22',
+
+
       },
       {
         spotId: 3,
         userId: 3,
         startDate: '2023-10-01',
         endDate: '2023-10-10',
+
+
       },])
   },
 
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-    options.tableName = 'Bookings';
-    const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(options, {
-      userId: {[Op.in]:[1,2,3]}
-    }, {});
-  }
+
+Add commands to revert seed here.*
+Example:
+await queryInterface.bulkDelete('People', null, {});*/
+options.tableName = 'Bookings';
+const Op = Sequelize.Op;
+return queryInterface.bulkDelete(options, {
+  userId: { [Op.in]: [1, 2, 3] }}, {});
+}
 };
